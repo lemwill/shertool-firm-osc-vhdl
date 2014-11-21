6549131b3a2181ae504ca078195216a627dec521
William Lemaire <william.lemaire@gmail.com>
2014-11-21 14:31:48 -0500

    ADDED IBUFDS

--- oscilloscope.srcs/sources_1/new/top_adc.vhd
+++ oscilloscope.srcs/sources_1/new/top_adc.vhd
@@ -26,8 +26,8 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+library UNISIM;
+use UNISIM.VComponents.all;
 
 entity top_adc is
     Port ( Clock100MHz : in  STD_LOGIC;
@@ -45,17 +45,54 @@ COMPONENT ClockPrescaler
           Clock1Hz : out  STD_LOGIC);
 END COMPONENT;
 
-signal adc_sig : std_logic;
-signal switch_sig : std_logic;
+signal data_chan1: STD_LOGIC_VECTOR (7 downto 0);
+signal data_chan2: STD_LOGIC_VECTOR (7 downto 0);
+signal data_clock: STD_LOGIC;
 
 
 begin
-	u1 : ClockPrescaler PORT MAP(Clock100MHz => Clock100MHz,
-								 Reset => '0',
-								 Clock1Hz => Led(0));
+    IBUFDS_CHAN1: for i in adc_data'range generate
+        INPUT_BUFFER: IBUFDS 
+        GENERIC MAP(
+            DIFF_TERM => true,
+            IOSTANDARD => "LVDS_25"
+        )
+        PORT MAP(
+            I  => ADC_CH1D_P(i),
+            IB => ADC_CH1D_N(i),
+            O  => data_chan1(i)
+        );
+    end generate;
+    
+    IBUFDS_CHAN2: for i in adc_data'range generate
+        INPUT_BUFFER: IBUFDS 
+        GENERIC MAP(
+            DIFF_TERM => TRUE,
+            IOSTANDARD => "LVDS_25"
+        )
+        PORT MAP(
+            I  => ADC_CH1D_P(i),
+            IB => ADC_CH1D_N(i),
+            O  => data_chan2(i)
+        );
+    end generate;
+        
+    DIFF_CLK_BUFFER : IBUFGDS
+    generic map (
+       DIFF_TERM => TRUE,
+       IOSTANDARD => "DEFAULT")
+    port map (
+       O => O, 
+       I => I, 
+       IB => IB 
+    );
+            
+	CLOCK_PRESCALER_1 : ClockPrescaler PORT MAP(Clock100MHz => Clock100MHz,
+			                                    Reset => '0',
+								                Clock1Hz => Led(0));
 
    --adc_sig <= ADC_CH1D_P(0);
-   Led(1) <= ADC_CH1D_P(0);
+   --Led(1) <= adc_data(0);
    
    --switch_sig <= Switch;
    Led(2) <= Switch;
